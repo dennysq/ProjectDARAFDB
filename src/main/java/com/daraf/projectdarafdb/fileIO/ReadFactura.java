80c739134cf8425a866295cd108565a7e4581593
Dennys Quiroz <dennysaurio@gmail.com>
2015-11-26 08:30:41 -0500

    ingresar factura , validando que no este repetida

--- src/main/java/com/daraf/projectdarafdb/fileIO/ReadFactura.java
+++ src/main/java/com/daraf/projectdarafdb/fileIO/ReadFactura.java
@@ -14,43 +14,56 @@ import java.util.List;
  *
  * @author ShipO
  */
-public class ReadFactura 
-{
-    public void leer(List<Factura> facturas) 
-    {
-        try
-        {          
+public class ReadFactura {
+
+    public void leer(List<Factura> facturas) {
+        try {
             String datos[];
             String cadena;
-            BufferedReader bf = new BufferedReader(new FileReader("Factura.txt"));         
-            while ((cadena = bf.readLine())!=null) 
-            {
+            BufferedReader bf = new BufferedReader(new FileReader("Factura.txt"));
+            while ((cadena = bf.readLine()) != null) {
                 System.out.println(cadena);
                 datos = cadena.split("\t");
                 System.out.println(cadena);
-                facturas.add(new Factura(datos[0],datos[1],datos[2],datos[3]));
+                facturas.add(new Factura(datos[0], datos[1], datos[2], datos[3]));
             }
-            
+
             bf.close();
-        }
-        catch (Exception e)
-        {
+        } catch (Exception e) {
             System.err.println("Ocurrio un error: " + e.getMessage());
         }
     }
-    
-    public void buscar(List<Factura> facturas, String id)
-    {
-        for (int i = 0; i < facturas.size(); i++) 
-        {
-                if(!facturas.get(i).getId().equals(id))
-                {
-                    facturas.remove(i);
+
+    public boolean verificaExistenciaFactura(String id) {
+        boolean respuesta = false;
+        try {
+            String datos[];
+            String cadena;
+            BufferedReader bf = new BufferedReader(new FileReader("factura.txt"));
+            while ((cadena = bf.readLine()) != null) {
+                datos = cadena.split("\t");
+                if (datos[0].equals(id)) {
+                    respuesta = true;
+                    break;
                 }
+
+            }
+
+            bf.close();
+        } catch (Exception e) {
+            System.err.println("Ocurrio un error: " + e.getMessage());
         }
-        for (int i = 0; i < facturas.size(); i++) 
-        {
-            System.out.println (facturas.get(i).toString());
+        return respuesta;
+    }
+
+    public void buscar(List<Factura> facturas, String id) {
+        for (int i = 0; i < facturas.size(); i++) {
+            if (!facturas.get(i).getId().equals(id)) {
+                facturas.remove(i);
+            }
+        }
+        for (int i = 0; i < facturas.size(); i++) {
+            System.out.println(facturas.get(i).toString());
         }
     }
 }
